91963926fb4e99695b8cca7a7fd8764f61eb5423
Matt McFarland <matmcfarland@gmail.com>
2013-07-25 10:19:06 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Remove built directory
+    clean: {
+      build: ['build/']
+    },
+
+    // Built stylesheets with less
+    less: {
+      build: {
+        src: 'assets/less/*',
+        dest: 'build/css/app.css'
+      }
+    },
+
+    // Build the site using grunt-includes
+    includes: {
+      build: {
+        cwd: 'site',
+        src: [ '*.html', 'pages/*.html' ],
+        dest: 'build/',
+        options: {
+          flatten: true,
+          banner: '<!-- Site built using grunt includes! -->\n'
+        }
+      }
+    }
+  });
+
+  // Load plugins used by this task gruntfile
+  grunt.loadNpmTasks('grunt-includes');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  // Task definitions
+  grunt.registerTask('build', ['clean', 'less', 'includes']);
+  grunt.registerTask('default', ['build']);
+};
